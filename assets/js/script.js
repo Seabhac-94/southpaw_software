3e40a964c90bb5b828bb52c273353da0ab7c74a8
Zachary Allen <zac_allen@Apples-MacBook-Pro.local>
2021-03-20 14:29:03 +0100

    adjust image path

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@ var button = $("button.button-1")
 var span = $("span.span-1")
 
 $(button).on("click", function (event) {
-    span.html("Rok's a bitch");
+    span.html(" ");
     span.addClass("py-2 px-2")
 })
