c13bc15a775aa9e69140e707802a9b1384da6b2b
Zachary Allen <zac_allen@Apples-MacBook-Pro.local>
2021-03-21 12:17:48 +0100

    fixed path to script.js and started js function for animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,13 @@ $(document).ready(function () {
 
     //Start functions here
     for (let i = 1; i < 21; i++) {
-        $(".animation-container").append("<div id='ball-" + i + "' class='ball-animation'></div>")
-    }
+        $(".animation-container").append("<div id='ball-" + i + "' class='ball-animation'></div>");
+        // if (i % 2 == 0) {
+        //     $("#ball-"+i).css({
+        //         "backgroundColor": "red",
+        //         "width": "10rem"
+        // })
+        // }
+    };
+
 });
