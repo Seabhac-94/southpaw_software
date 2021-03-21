a5811dba73a0be8c6b2a086c0afcccf30f23ced4
Zachary Allen <zac_allen@Apples-MacBook-Pro.local>
2021-03-21 18:32:10 +0100

    wrote script to cut out 800 lines of CSS, now reusing keyframes for ball-1 and ball-2 and using javascrpt to assign depending on if i%2==0 and incrementing animation delay using i*0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
-    console.log("ready!");
+console.log("ready!");
 
-    //Start functions here
-    for (let i = 1; i < 21; i++) {
+    //Function to loop through 1-50 and create the animation balls
+    //Balls use same 2 keyframes from animation.css but delay is incremented by 0.1 to create wave effect
+    for (let i = 1; i < 51; i++) {
         $(".animation-container").append("<div id='ball-" + i + "' class='ball-animation'></div>");
+        if (i % 2 == 0) {
+            $("#ball-" + i).css({
+                "animationName": "ball-2",
+            });
+        } else
+            $("#ball-" + i).css({
+                "animationName": "ball-1",
+            });
         $("#ball-" + i).css({
-            "animationName": "ball-" + i,
-            "animationDelay": `${i * 0.2}s`,
+            "animationDelay": `${i * 0.1}s`,
             "top": "-2rem",
             "left": "-2%"
         });
     };
-});
+    });
