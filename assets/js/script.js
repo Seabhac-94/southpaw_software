c9d380ed778ced6f5de7376109f2294888f9fc40
Zachary Allen <zac_allen@Apples-MacBook-Pro.local>
2021-03-24 20:27:42 +0100

    added media queries for +1600px, removed slide animation, replaced with fade-in, increased count of animation for continuous flow accross screen, minor changes on timings, old code commented out in case of revisit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-console.log("ready!");
+    console.log("ready!");
 
     //Function to loop through 1-50 and create the animation balls
     //Balls use same 2 keyframes from animation.css but delay is incremented by 0.1 to create wave effect
-    for (let i = 1; i < 51; i++) {
+    for (let i = 1; i < 80; i++) {
         $(".animation-container").append("<div id='ball-" + i + "' class='ball-animation'></div>");
         if (i % 2 == 0) {
             $("#ball-" + i).css({
@@ -18,5 +18,8 @@ console.log("ready!");
             "top": "-2rem",
             "left": "-2%"
         });
+        //change for final iteration only
+        console.log([i]+"|"+[i*0.1])
     };
-    });
+
+});
