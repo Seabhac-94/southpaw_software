ee7d0d9787120dd5258894f80dd1e6eac87e411b
Zachary Allen <zac_allen@Apples-MacBook-Pro.local>
2021-03-27 12:01:16 +0100

    switched js code to template literal, added and styled navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ $(document).ready(function () {
 
     //Function to loop through 1-50 and create the animation balls
     //Balls use same 2 keyframes from animation.css but delay is incremented by 0.1 to create wave effect
+
     for (let i = 1; i < 80; i++) {
-        $(".animation-container").append("<div id='ball-" + i + "' class='ball-animation'></div>");
+        $("#animation-container").append(`<div id=ball-${i} class=ball-animation></div>`);
         if (i % 2 == 0) {
             $("#ball-" + i).css({
                 "animationName": "ball-2",
